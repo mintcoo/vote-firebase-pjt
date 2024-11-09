31bf8580f84a2160c4294cd80761b6780a16c15a
HanSangHyun <xhaktm3344@gmail.com>
2024-11-09 16:06:14 +0900

    테일윈드 세팅, 폰트 세팅

--- vote-web/src/App.tsx
+++ vote-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div></div>;
+  return <div className="text-4xl font-bold">테일윈드 되었냐</div>;
 }
 
 export default App;
---
+++ vote-web/src/Router.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <Routes>
+      {/* <Route path="" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/complete" element={<Complete />} />
+        <Route path="/error" element={<Error />} />
+      </Route> */}
+    </Routes>
+  );
+}
+
+export default Router;
